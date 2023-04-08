c57502d12c49ef02261f38101e4e282869a7ac16
seeewhy <85683554+seeewhy@users.noreply.github.com>
2023-04-08 18:39:19 +0100

    Create install_jenkins.sh

---
+++ install_jenkins.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+sudo apt-get update -y
+
+# Install Java SDK 11
+sudo apt-get install openjdk-11-jdk -y
+
+# Download and Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
+
+# download and install Maven Build Tool
+
+sudo apt install maven -y
+
+# Start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
